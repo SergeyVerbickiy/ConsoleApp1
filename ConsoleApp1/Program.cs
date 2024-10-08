b4b7558829b40a63144f694da442c2c6a7f62d2e
Sergey <seregav666@gmail.com>
2024-10-08 13:56:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+
+            Console.WriteLine("Для выхода нажмите клавишу Enter");
+
+            Console.ReadKey();
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                while (keyInfo.Key != ConsoleKey.Enter)
+                    keyInfo = Console.ReadKey();
+            }
+        }
+    }
+}
